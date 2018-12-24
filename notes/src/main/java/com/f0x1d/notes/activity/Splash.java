21ce72354fdf4ad422205d570b062eb4e16ff250
F0x1d <maks.zrandles@gmail.com>
2018-12-24 16:20:50 +0500

    something

--- notes/src/main/java/com/f0x1d/notes/activity/Splash.java
+++ notes/src/main/java/com/f0x1d/notes/activity/Splash.java
@@ -2,6 +2,9 @@ package com.f0x1d.notes.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
@@ -9,6 +12,11 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
