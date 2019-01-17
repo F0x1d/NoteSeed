7c250ca65fa625c5597cbe05f3a58d0dfd25c798
F0x1d <maks.zrandles@gmail.com>
2019-01-17 16:08:21 +0500

    fcm fixed

--- notes/src/main/java/com/f0x1d/notes/App.java
+++ notes/src/main/java/com/f0x1d/notes/App.java
@@ -2,12 +2,23 @@ package com.f0x1d.notes;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
+import com.f0x1d.notes.activity.MainActivity;
 import com.f0x1d.notes.db.Database;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import androidx.annotation.NonNull;
 import androidx.room.Room;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
+import io.fabric.sdk.android.Fabric;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class App extends Application {
 
@@ -26,6 +37,15 @@ public class App extends Application {
     public void onCreate() {
         instance = this;
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.e("notes_err", "subscribed");
+                    }
+                });
 
         database = Room.databaseBuilder(this, Database.class, "database")
                 .allowMainThreadQueries()
--- notes/src/main/java/com/f0x1d/notes/receiver/FCMReceiver.java
+++ notes/src/main/java/com/f0x1d/notes/receiver/FCMReceiver.java
@@ -6,10 +6,14 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.f0x1d.notes.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Collection;
 import java.util.List;
@@ -24,38 +28,32 @@ public class FCMReceiver extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            String title = null;
+            String text = null;
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                String name1 = "Напоминания";
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                NotificationChannel channel = new NotificationChannel("com.f0x1d.notes", name1, importance);
-                // Register the channel with the system; you can't change the importance
-                // or other notification behaviors after this
-                channel.enableVibration(true);
-                channel.enableLights(true);
-                NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                notificationManager.createNotificationChannel(channel);
+            JSONObject jsonObject = new JSONObject(remoteMessage.getData());
+
+            Log.e("notes_err", jsonObject.toString());
+
+            try {
+                title = jsonObject.getString("title");
+                text = jsonObject.getString("body");
+
+                Notification.Builder builder = new Notification.Builder(getApplicationContext());
+                builder.setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setAutoCancel(true)
+                        .setVibrate(new long[]{1000L, 1000L, 1000L})
+                        .setChannelId("com.f0x1d.notes");
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(99999, builder.build());
+            } catch (JSONException e) {
+                Log.e("notes_err", e.getLocalizedMessage());
             }
-
-            Notification.Builder builder = new Notification.Builder(getApplicationContext());
-            builder.setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
-                    .setContentTitle(remoteMessage.getData().get("title"))
-                    .setContentText(remoteMessage.getData().get("body"))
-                    .setAutoCancel(true)
-                    .setVibrate(new long[]{1000L, 1000L, 1000L})
-                    .setChannelId("com.f0x1d.notes");
-
-            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            manager.notify(99999, builder.build());
-        }
     }
 
     @Override
