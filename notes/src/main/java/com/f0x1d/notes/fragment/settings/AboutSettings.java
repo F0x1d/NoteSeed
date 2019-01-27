6518dfbc6f30fb516e31bd51dbbc0f469393a5b7
F0x1d <maks.zrandles@gmail.com>
2019-01-27 12:47:39 +0500

    fix

---
+++ notes/src/main/java/com/f0x1d/notes/fragment/settings/AboutSettings.java
@@ -0,0 +1,58 @@
+package com.f0x1d.notes.fragment.settings;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+
+import com.f0x1d.notes.R;
+import com.f0x1d.notes.utils.ThemesEngine;
+import com.f0x1d.notes.utils.UselessUtils;
+import com.f0x1d.notes.view.CenteredToolbar;
+
+public class AboutSettings extends PreferenceFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.settings, container, false);
+
+        CenteredToolbar toolbar = v.findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.settings));
+        getActivity().setActionBar(toolbar);
+
+        if (UselessUtils.ifCustomTheme()){
+            getActivity().getWindow().setBackgroundDrawable(new ColorDrawable(ThemesEngine.background));
+            getActivity().getWindow().setStatusBarColor(ThemesEngine.statusBarColor);
+            getActivity().getWindow().setNavigationBarColor(ThemesEngine.navBarColor);
+
+            if (ThemesEngine.toolbarTransparent){
+                toolbar.setBackgroundColor(ThemesEngine.toolbarColor);
+            }
+        }
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.about);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        View rootView = getView();
+        if (rootView != null) {
+            ListView list = (ListView) rootView.findViewById(android.R.id.list);
+            list.setPadding(0, 0, 0, 0);
+            list.setDivider(null);
+        }
+    }
+}
