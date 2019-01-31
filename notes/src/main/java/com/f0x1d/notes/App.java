d38ccefcbb2e1a23076974dbd2a595e3f18ae61d
F0x1d <maks.zrandles@gmail.com>
2019-01-31 15:59:09 +0500

    deleted fcm

--- notes/src/main/java/com/f0x1d/notes/App.java
+++ notes/src/main/java/com/f0x1d/notes/App.java
@@ -6,9 +6,6 @@ import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 import com.f0x1d.notes.db.Database;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import androidx.annotation.NonNull;
 import androidx.room.Room;
@@ -34,14 +31,6 @@ public class App extends Application {
         super.onCreate();
 
         Fabric.with(this, new Crashlytics());
-
-        FirebaseMessaging.getInstance().subscribeToTopic("all")
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.e("notes_err", "subscribed");
-                    }
-                });
 
         database = Room.databaseBuilder(this, Database.class, "noteseed_db")
                 .allowMainThreadQueries()
